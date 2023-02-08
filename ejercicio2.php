4d9d172e079e25fc95c138734f69e2e3d156f52c
Emmanuel Solis <emma.ruso@outlook.com>
2023-02-08 09:58:03 -0600

    Agregamos el request de la palabra al ejercicio 2 palindromo

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,10 +1,19 @@
+
 <?php
-
-function espalindromo($cadena){
-    $array = str_split($cadena);
-    $voltear=array_reverse($array);
-
-    return $cadena == implode("",$voltear) ? "Es palindromo" : "No es palindromo";
+if (isset($_POST['enviar'])){
+    $palabra=$_POST['palabra'];
+    //Creamos una función que recibe una palabra de texto y devuelve si es palíndromo o no.
+    function espalindromo($palabra){
+        //Creamos un array con las letras de la palabra de texto 
+        $array = str_split($palabra);
+        //Invertimos el array
+        $voltear=array_reverse($array);
+        //Comparamos si la palabra de texto es igual al array invertido
+        return $palabra == implode("",$voltear) ? "$palabra, es un palindromo" : "$palabra, no es palindromo";
+    }
+    
+    echo espalindromo("$palabra");
 }
-
-echo espalindromo("anitalavalatina");
+?>
+<br><br>
+<a href="index.html">Regresar a home</a>
